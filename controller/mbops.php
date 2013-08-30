0c5ff697cbb3030a3f3dcababf9bcff8801c6d2d
kelaocai <kelaocai@163.com>
2013-08-30 17:49:22 +0800

    mbops调整格式，优化取数逻辑

--- controller/mbops.php
+++ controller/mbops.php
@@ -15,21 +15,19 @@ class mbops extends spController {
 		//echo($today);
 		$db = spDB("DDZ_TAOKE_ORDER_LOG");
 		$conditon= array("date(GMT_CREATE)"=>$today,"STATUS"=>"S","TYPE"=>'UP');
-		$rs= $db -> find($conditon, "GMT_CREATE DESC", null);
-		$rs_dtl=json_decode($rs['DETAIL']);
+		$rs_order= $db -> find($conditon, "GMT_CREATE DESC", null);
+		$rs_dtl=json_decode($rs_order['DETAIL']);
 		//dump($rs_dtl);
-		$this -> ttl_real_cnt=$rs_dtl->{'tradeCount'};
+		$this -> ttl_real_cnt=$rs_dtl->{'payCount'};
 		$this -> ttl_real_commission=$rs_dtl->{'commission'};
-		$this -> ttl_real_date=$rs['GMT_CREATE'];
+		$this -> ttl_real_date=$rs_order['GMT_CREATE'];
 		
 		//实时确认收货
-		//select sum(commission) from DDZ_TAOKE_REPORT where date(GMT_PAID)='2013-08-26'
 		$db = spDB("DDZ_TAOKE_REPORT");
 		$conditon= array("date(GMT_PAID)"=>$today);
-		$rs_cnt_confirm=$db->findCount($conditon);
-		$rs_sum_confirm=$db->find($conditon,null,"SUM(COMMISSION) comminsion");
-		$this -> ttl_cnt_confirm=$rs_cnt_confirm;
-		$this -> ttl_sum_confirm=$rs_sum_confirm['comminsion'];
+		$rs_report=$db->find($conditon,null,"SUM(COMMISSION) comminsion,count(*) cnt");
+		$this -> ttl_cnt_confirm=$rs_report['cnt'];
+		$this -> ttl_sum_confirm=$rs_report['comminsion'];
 		
 
 	}
