edc2452016a0066c2f2ad6a2517ef4f159d1fecb
kelaocai <kelaocai@163.com>
2013-09-11 21:04:55 +0800

    fix sq

--- controller/sqds.php
+++ controller/sqds.php
@@ -13,14 +13,14 @@ class sqds extends spController {
 		$alipay_id = trim($this -> spArgs('alipay_id'));
 		$phone_no = trim($this -> spArgs('phone_no'));
 		$sn = trim($this -> spArgs('sn'));
-		$rs_flag=FALSE;
+		$rs_flag = FALSE;
 
 		if (strcmp('', $alipay_id) == 0) {
 			$this -> msg = "请提供参赛支付宝帐号!";
 			return;
 		} else {
 			if (!($this -> checkEmail($alipay_id) || $this -> checkMobile($alipay_id))) {
-				$this -> msg = "不是合法的支付宝帐号!";
+				$this -> msg = "不是有效的支付宝帐号!";
 				return;
 			}
 		}
@@ -33,18 +33,25 @@ class sqds extends spController {
 		if (strcmp('', $phone_no) == 0) {
 			$this -> msg = "请提供手机号码!";
 			return;
+		} else {
+			if (!$this -> checkMobile($phone_no)) {
+				$this -> msg = "不是有效的手机号!";
+				return;
+			}
 		}
 
 		$db_ddz_account = spDB("DDZ_ACCOUNT");
 		$condition = array('alipay_id' => $alipay_id);
 		$rs_account = $db_ddz_account -> find($condition);
 		if ($rs_account) {
-			$uid = $rs['ID'];
-			$spm = $rs['REG_SPM'];
+			$uid = $rs_account['ID'];
+			$spm = $rs_account['REG_SPM'];
+
 		}
+		
 
 		//如果没有spm标识,归为大赛推荐
-		if ($uid > 0 & strcasecmp('', $spm) == 0) {
+		if (($uid > 0) & ((strcasecmp('', $spm) == 0 )|| empty($spm))) {
 			$up_condition = array('id' => $uid);
 			$up_content = array('REG_SPM' => $spm_mark);
 			$up_rs = $db_ddz_account -> update($up_condition, $up_content);
@@ -56,13 +63,13 @@ class sqds extends spController {
 		$rs_sq_reg = $db_sq_reg -> find($condition_reg);
 		if (!$rs_sq_reg) {
 			$now = date("Y-m-d H:i:s", time());
-			$new_sq_reg = array("ALIPAY_ID" => $alipay_id, "PHONE_NO" => $phone_no, 'GMT_CREATE' => $now, 'STATUS' => '1');
+			$new_sq_reg = array("ALIPAY_ID" => $alipay_id, "PHONE_NO" => $phone_no, 'SN' => $sn, 'GMT_CREATE' => $now, 'STATUS' => '1');
 			$create_rs = $db_sq_reg -> create($new_sq_reg);
 			if (!$create_rs) {
 				$this -> msg = "信息提交失败,请重试!";
 			} else {
 				$this -> msg = "报名成功!";
-				$this->rs_flag=TRUE;
+				$this -> rs_flag = TRUE;
 			}
 
 		} else {
