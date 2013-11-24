c944c45fc23fba921d045f95665fb67f66994030
kelaocai <kelaocai@163.com>
2013-11-24 15:40:59 +0800

    add wx port

---
+++ controller/wx.php
@@ -0,0 +1,32 @@
+<?php
+
+define("TOKEN", "1qaz2wsx");
+
+class mbops extends spController {
+	
+	function wxreader(){
+		
+	}
+	
+	
+	function check()
+	{
+        $signature = $_GET["signature"];
+        $timestamp = $_GET["timestamp"];
+        $nonce = $_GET["nonce"];	
+        		
+		$token = TOKEN;
+		$tmpArr = array($token, $timestamp, $nonce);
+		sort($tmpArr);
+		$tmpStr = implode( $tmpArr );
+		$tmpStr = sha1( $tmpStr );
+		
+		if( $tmpStr == $signature ){
+			return true;
+		}else{
+			return false;
+		}
+	}
+
+}
+?>
